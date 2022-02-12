07ec36d51f1e5c70d51dfb1c99533ce783651a9d
jackula83 <jackula@gmail.com>
2022-02-12 22:36:45 +1100

    progress

---
+++ FlagSense.FlagService.Api/Options/DatabaseOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace FlagSense.FlagService.Api.Options
+{
+    internal class DatabaseOptions
+    {
+        public const string OptionName = "Database";
+
+        public string Server { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public int Port { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
--- FlagSense.FlagService.Domain/Data/FsContext.cs
+++ FlagSense.FlagService.Domain/Data/FsContext.cs
@@ -1,12 +1,23 @@
 ﻿using Common.Domain.Core.Data;
+using FlagSense.FlagService.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace FlagSense.FlagService.Domain.Data
 {
     public class FsContext : FxSqlServerDbContext
     {
+        public DbSet<FsEnvironment> Environments { get; set; }
+        public DbSet<Flag> Flags { get; set; }
+        public DbSet<FlagValue> FlagValues { get; set; }
+        public DbSet<Rule> Rules { get; set; }
+        public DbSet<RuleGroup> RuleGroups { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserProperty> UserProperties { get; set; }
+
+#pragma warning disable 8618
         public FsContext(DbContextOptions options) : base(options)
         {
         }
+#pragma warning restore 8618
     }
 }
