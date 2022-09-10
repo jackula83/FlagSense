9905b76332f3c94f2654acc220304772089dc162
jackula83 <jackula@gmail.com>
2022-09-10 19:12:47 +1000

    added flag toggler and integrated framework tests

---
+++ src/FlagService.Api/Features/Flags/FlagToggleCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using FlagService.Domain.Aggregates.Flags;
+using Framework2.Core.Extensions;
+using Framework2.Domain.Core.Handlers;
+
+namespace FlagService.Api.Features.Flags
+{
+    public class FlagToggleCommandHandler : FxCommandHandler<FlagToggleCommandRequest, FlagToggleCommandResponse>
+    {
+        private readonly IFlagRepository _flagRepository;
+
+        public FlagToggleCommandHandler(IFlagRepository flagRepository, ILogger<FlagToggleCommandHandler> logger)
+            : base(logger)
+        {
+            _flagRepository = flagRepository;
+        }
+
+        protected override async Task<FlagToggleCommandResponse> ExecuteAsync(FlagToggleCommandRequest request, CancellationToken cancellationToken = default)
+        {
+            var flag = await _flagRepository.Get(request.FlagId);
+            if (flag == null)
+                return new();
+
+            flag.Tap(x => x.IsEnabled = !x.IsEnabled);
+            await _flagRepository.Update(flag);
+            await _flagRepository.Save();
+
+            return new FlagToggleCommandResponse { Flag = flag };
+        }
+    }
+}
---
+++ tests/FlagService.Api.UnitTests/Features/Flags/FlagToggleCommandHandlerTest.cs
@@ -0,0 +1,94 @@
+﻿using FlagService.Api.Features.Flags;
+using FlagService.Domain.Aggregates;
+using FlagService.Domain.Aggregates.Flags;
+using FlagService.Domain.Auditing;
+using FlagService.Domain.Contexts;
+using FlagService.Infra.Data.Repositories;
+using Framework2.Infra.Data.UnitTests.Utilities;
+using Framework2.Infra.MQ.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FlagService.Api.UnitTests.Features.Flags
+{
+    public class FlagToggleCommandHandlerTest
+    {
+        private readonly FlagToggleCommandHandler _sut;
+        private readonly FsSqlServerContext _context;
+        private readonly AuditOperations _auditOperations;
+        private readonly IFlagRepository _flagRepository;
+        private readonly Mock<IEventQueue> _eventQueueMock;
+        private readonly NullLogger<FlagToggleCommandHandler> _logger;
+
+        public FlagToggleCommandHandlerTest()
+        {
+            _eventQueueMock = new();
+            _logger = NullLogger<FlagToggleCommandHandler>.Instance;
+            _context = Utils.CreateInMemoryDatabase<FsSqlServerContext>(nameof(FlagToggleCommandHandlerTest))!;
+            _auditOperations = new(_eventQueueMock.Object, _context);
+            _flagRepository = new FlagRepository(_context, _auditOperations);
+            _sut = new(_flagRepository, _logger);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task Handle_GivenFlag_ShouldReturnFlagWithNegatedIsEnabled(bool isEnabled)
+        {
+            // arrange
+            var flag = new Flag
+            {
+                Id = 1,
+                IsEnabled = isEnabled
+            };
+            await _flagRepository.Add(flag);
+            await _flagRepository.Save();
+
+            // act
+            var response = await _sut.Handle(new() { FlagId = flag.Id });
+
+            // assert
+            Assert.NotNull(response?.Flag);
+            Assert.Equal(response!.Flag!.IsEnabled, !isEnabled);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task Handle_GivenFlag_ResultShouldBeSavedInRepository(bool isEnabled)
+        {
+            // arrange
+            var flag = new Flag
+            {
+                Id = 1,
+                IsEnabled = isEnabled
+            };
+            await _flagRepository.Add(flag);
+            await _flagRepository.Save();
+
+            // act
+            await _sut.Handle(new() { FlagId = flag.Id });
+            _context.ChangeTracker.Clear();
+            var flagFromRepository = await _flagRepository.Get(flag.Id);
+
+            // assert
+            Assert.NotNull(flagFromRepository);
+            Assert.Equal(flagFromRepository!.IsEnabled, !isEnabled);
+        }
+
+        [Fact]
+        public async Task Handle_GivenFlagDoesntExist_ResultReturnNullFlag()
+        {
+            // act
+            var response = await _sut.Handle(new() { FlagId = 1 });
+
+            // assert
+            Assert.NotNull(response);
+            Assert.Null(response!.Flag);
+        }
+    }
+}
